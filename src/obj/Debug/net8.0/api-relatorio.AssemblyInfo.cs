50cd60506c5d5e926df96761fb763c014f6becd2
Cickruss <icaro.o.macedo@gmail.com>
2024-07-16 20:38:14 -0300

    ajustes de repositorio

--- src/obj/Debug/net8.0/api-relatorio.AssemblyInfo.cs
+++ src/obj/Debug/net8.0/api-relatorio.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api-relatorio")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+5c73b94e40dd47ec04bca7cc75769a9f3e1b3d7b")]
 [assembly: System.Reflection.AssemblyProductAttribute("api-relatorio")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api-relatorio")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
